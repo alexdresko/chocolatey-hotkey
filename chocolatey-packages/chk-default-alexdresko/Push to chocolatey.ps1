3db7ef5221d2fd6c0d7d353042682dd480dc1354
Alex Dresko <me@alexdresko.com>
2015-11-29 13:47:01 -0500

    Added to chk-default-alexdresko.

--- chocolatey-packages/chk-default-alexdresko/Push to chocolatey.ps1
+++ chocolatey-packages/chk-default-alexdresko/Push to chocolatey.ps1
@@ -1,1 +1,6 @@
-choco push
+$name = (get-item .).Name
+$source = (get-item .).FullName
+[xml]$nuspec = Get-Content "$name.nuspec"
+$version = $nuspec.DocumentElement.metadata.version
+write-host "pushing $name.$version.nupkg"
+choco push "$name.$version.nupkg"
