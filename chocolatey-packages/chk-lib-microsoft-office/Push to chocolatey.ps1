c912249daf41625389a2cfb482143cfc3843080b
Alex Dresko <me@alexdresko.com>
2015-12-29 20:29:31 -0500

    Repushed all packages and noticed a few small errors here.

--- chocolatey-packages/chk-lib-microsoft-office/Push to chocolatey.ps1
+++ chocolatey-packages/chk-lib-microsoft-office/Push to chocolatey.ps1
@@ -1,8 +1,8 @@
+$thisDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+cd $thisDir
 $name = (get-item .).Name
 $source = (get-item .).FullName
 [xml]$nuspec = Get-Content "$name.nuspec"
 $version = $nuspec.DocumentElement.metadata.version
 write-host "pushing $name.$version.nupkg"
-$thisDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-cd $thisDir
 choco push "$name.$version.nupkg"
